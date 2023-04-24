5a0ff23fbb0591199a3646f3a69644788ca17fb9
roger-josue <jovz1798@gmail.com>
2023-04-24 15:58:50 -0600

    Axios instance now pointing to the deployed version of the api

--- src/API/booksAPI.js
+++ src/API/booksAPI.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const booksApi = axios.create({
-    baseURL: "http://localhost:8000/",
+    baseURL: "https://books-api-prod.up.railway.app/",
     headers: {'Accept': 'application/json'}
 });
 
